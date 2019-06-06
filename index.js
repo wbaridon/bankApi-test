b080b4be9e9e5aaecceb1bdf0684000ba6aa7425
Wenceslas BARIDON <wbaridon@e3r1p9.42.fr>
2019-06-06 09:28:52 +0200

    Add function listAccounts and sumAccounts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
+axios.defaults.headers.common['Bankin-Version'] = '2018-06-15'
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -11,13 +12,29 @@ app.get('/', (req, res) => {
 })
 
 function bridgeAuth (email, password) {
-    let baseUrl = 'https://sync.bankin.com/v2'
     let params = `email=${email}&password=${password}&client_id=${process.env.clientId}&client_secret=${process.env.secretId}`
-    console.log(params)
-    axios.post(`${baseUrl}/authenticate?${params}`)
+    axios.post(`${process.env.bridgeUrl}/authenticate?${params}`)
     .then(res => {
         console.log(res)
     })
+}
+
+function listAccounts () {
+    var config = {
+        headers: {'Authorization': "bearer " + token}
+    };
+    axios.post(`${process.env.bridgeUrl}/accounts?client_id=${process.env.clientId}&client_secret=${process.env.secretId}`, config)
+    .then(accounts => {
+        sumAccounts(accounts.data)
+    })
+}
+
+function sumAccounts(accounts) {
+    let balance = 0
+    for (let i = 0; i < accounts.ressources.length; i++) {
+        balance += accounts.resources[i].balance
+    }
+    console.log(balance)
 }
 
 app.listen(port, () => console.log(`BankApi app listening on port ${port}!`))
