2710a08bd3d0aba83d7a9f8c89b7318d94108ec0
Wenceslas BARIDON <wbaridon@e3r1p9.42.fr>
2019-06-08 12:11:09 +0200

    A small technical test

--- index.js
+++ index.js
@@ -6,35 +6,46 @@ const port = 3000
 
 app.get('/', (req, res) => {
     let email = 'user2@mail.com'
-    let password = 'a!Strongp#assword2'
-    bridgeAuth(email, password)
-    console.log(process.env.clientId)
+    let password = encodeURIComponent('a!Strongp#assword2')
+    bridgeAuth(email, password).then(balance => {
+        res.status(200).send({balance})
+    })
 })
 
 function bridgeAuth (email, password) {
-    let params = `email=${email}&password=${password}&client_id=${process.env.clientId}&client_secret=${process.env.secretId}`
-    axios.post(`${process.env.bridgeUrl}/authenticate?${params}`)
-    .then(res => {
-        console.log(res)
+    return new Promise ((resolve) => {
+        let params = `email=${email}&password=${password}&client_id=${process.env.clientId}&client_secret=${process.env.secretId}`
+        axios.post(`${process.env.bridgeUrl}/authenticate?${params}`)
+        .then(res => {
+            listAccounts(res.data['access_token']).then(sum => {
+                resolve(sum)
+            })
+        })         
     })
 }
 
-function listAccounts () {
-    var config = {
-        headers: {'Authorization': "bearer " + token}
-    };
-    axios.post(`${process.env.bridgeUrl}/accounts?client_id=${process.env.clientId}&client_secret=${process.env.secretId}`, config)
-    .then(accounts => {
-        sumAccounts(accounts.data)
+function listAccounts (token) {
+    return new Promise ((resolve) => {
+        var config = {
+            headers: {'Authorization': "Bearer " + token}
+        };
+        axios.get(`${process.env.bridgeUrl}/accounts?client_id=${process.env.clientId}&client_secret=${process.env.secretId}`, config)
+        .then(accounts => {
+            sumAccounts(accounts.data).then(balance => {
+                resolve(balance)
+            })
+        })
     })
 }
 
 function sumAccounts(accounts) {
-    let balance = 0
-    for (let i = 0; i < accounts.ressources.length; i++) {
-        balance += accounts.resources[i].balance
-    }
-    console.log(balance)
+    return new Promise ((resolve) => {
+        let balance = 0
+        for (let i = 0; i < accounts.resources.length; i++) {
+            balance += accounts.resources[i].balance
+        }
+        resolve(balance)
+    })
 }
 
 app.listen(port, () => console.log(`BankApi app listening on port ${port}!`))
